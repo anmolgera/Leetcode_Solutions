9faece31bb7dc40dd5ea32e1d372514ad6e40b8b
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-05-13 13:17:34 +0000

    Sync LeetCode submission - Defanging an IP Address (cpp)

---
+++ AG_Leetcode/problems/defanging_an_ip_address/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string res ="";
+        
+        for(int i =0; i<address.length(); i++){
+            
+            
+            
+           if(address[i]!='.'){
+              res +=address[i]; 
+               
+           } 
+            
+        else res+="[.]";
+            
+            
+            
+            
+            
+        }
+        
+      return res;  
+        
+        
+        
+        
+    }
+};
