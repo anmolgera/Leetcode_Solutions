31082032e911b176576e228cd10553670ac0136c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-05-18 20:10:41 +0000

    Sync LeetCode submission - Reverse Linked List (cpp)

---
+++ AG_Leetcode/problems/reverse_linked_list/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+       ListNode* current = head; 
+        ListNode *prev = NULL, *next = NULL; 
+        while(current!=NULL){
+           next = current->next; 
+  
+            // Reverse current node's pointer 
+            current->next = prev; 
+  
+            // Move pointers one position ahead. 
+            prev = current; 
+            current = next; 
+        } 
+            
+            
+        
+        
+        return prev;
+    }
+};
