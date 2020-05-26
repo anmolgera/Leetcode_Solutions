c4c212c460c7ce5d7d121dd82873093caf1ad86f
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-05-26 19:16:14 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

---
+++ AG_Leetcode/problems/maximum_depth_of_binary_tree/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        
+     if(root == NULL){
+         return 0;
+     }   
+    int ans =0;
+        
+        
+    int smallans = maxDepth(root->left);
+    int smallans2 = maxDepth(root->right);
+        
+        
+    return max(smallans,smallans2) +1;
+        
+        
+        
+        
+        
+    }
+};
