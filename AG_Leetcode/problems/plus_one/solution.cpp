283ac4ce9ed35625582408a8cc7acd8205d7bc5c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-06-03 00:26:24 +0000

    Sync LeetCode submission - Plus One (cpp)

---
+++ AG_Leetcode/problems/plus_one/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+      int n = digits.size();
+        
+       digits[n-1] +=1;
+       int carry = digits[n-1]/10;
+        digits[n-1] = digits[n-1]%10;
+        
+        
+       for(int i = n-2; i>=0; i--){
+           
+           if(carry ==1){
+               
+               digits[i] +=1;
+       carry = digits[i]/10;
+        digits[i] = digits[i]%10;
+        
+               
+               
+           }
+           
+           
+           
+           
+           
+           
+       }
+        
+        
+        
+    if(carry ==1){
+        digits.insert(digits.begin(),1);
+        
+        
+    }
+        
+        
+     return digits;   
+        
+        
+        
+        
+    }
+};
