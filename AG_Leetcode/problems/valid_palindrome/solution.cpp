3a90072da24935521fb08a7222504c44064e529d
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-06-04 13:33:34 +0000

    Sync LeetCode submission - Valid Palindrome (cpp)

---
+++ AG_Leetcode/problems/valid_palindrome/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int i =0;
+        int j = s.length()-1;
+        
+        while(i<=j){
+            
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            
+            if((!isalnum(s[j]))){
+                
+                j--;
+                continue;
+            }
+            
+           
+            if(tolower(s[i++])!=tolower(s[j--])){
+                return false;
+            }
+            
+        }
+        
+        
+      return true;  
+        
+    }
+};
