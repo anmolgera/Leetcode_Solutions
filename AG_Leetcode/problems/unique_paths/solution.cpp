743492de9ef88c21a860b729f8f0e394d7cd5f8c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-02 22:07:51 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- AG_Leetcode/problems/unique_paths/solution.cpp
+++ AG_Leetcode/problems/unique_paths/solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-      int count[m][n]; 
-  
-    // Count of paths to reach any cell in first column is 1 
-    for (int i = 0; i < m; i++) 
-        count[i][0] = 1; 
-  
-    // Count of paths to reach any cell in first row is 1 
-    for (int j = 0; j < n; j++) 
-        count[0][j] = 1; 
-  
-    // Calculate count of paths for other cells in 
-    // bottom-up manner using the recursive solution 
-    for (int i = 1; i < m; i++) { 
-        for (int j = 1; j < n; j++) 
-  
-            // By uncommenting the last part the code calculatest he total 
-            // possible paths if the diagonal Movements are allowed 
-            count[i][j] = count[i - 1][j] + count[i][j - 1]; //+ count[i-1][j-1]; 
-    } 
-    return count[m - 1][n - 1]; 
+        
+        long long int count =1;
+        
+        
+        for(int i =n; i<n+m-1; i++){
+            
+           count = count*i;
+            count = count/(i-n+1);
+            
+            
+            
+        }
+        
+       return count; 
+        
+        
     }
 };
