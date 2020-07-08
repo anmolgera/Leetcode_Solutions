83387bfe28fec36f00231d2a4759fbd8d9436be0
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-08 21:49:28 +0000

    Sync LeetCode submission - Number of Substrings Containing All Three Characters (cpp)

---
+++ AG_Leetcode/problems/number_of_substrings_containing_all_three_characters/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        
+      int count[3] = {0,0,0};
+        
+        
+    int ans =0;
+        int j=0;
+      for(int i =0; i<s.length(); i++){
+          
+          count[s[i]-'a']++;
+          
+          
+          while(count[0]&& count[1] && count[2]){
+              
+              count[s[j]-'a']--;
+              //ans +=i;
+              j++;
+              
+          }
+          
+          ans +=j;
+          
+      }
+       return ans; 
+        
+    }
+};
