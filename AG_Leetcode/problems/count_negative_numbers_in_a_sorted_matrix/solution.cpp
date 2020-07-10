40dfc7e6cc43afa6e0cf2a7b5c51d4ff5e3ab977
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-10 20:33:44 +0000

    Sync LeetCode submission - Count Negative Numbers in a Sorted Matrix (cpp)

--- AG_Leetcode/problems/count_negative_numbers_in_a_sorted_matrix/solution.cpp
+++ AG_Leetcode/problems/count_negative_numbers_in_a_sorted_matrix/solution.cpp
@@ -2,16 +2,34 @@ class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
         
-       int res = 0;
-    for(int i=grid.size()-1;i>=0;--i){
-        for(int j=grid[i].size()-1;j>=0;--j){
-            if (grid[i][j] >= 0){
-               break;
+        
+        int count =0;
+        for(int i =0; i<grid.size(); i++){
+            
+            for(int j =0; j<grid[i].size(); j++){
+                
+                
+                if(grid[i][j]<0){
+                    
+                    
+                    count++;
+                    
+                }
+                
+                
+                
             }
-            res++;
+            
+            
+            
+            
+            
+            
         }
-    }
-    
-    return res;
+        
+        
+      return count;  
+        
+        
     }
 };
