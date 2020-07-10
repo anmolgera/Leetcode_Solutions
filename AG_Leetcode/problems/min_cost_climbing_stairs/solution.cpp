f6baa3360c0aeebb98095fa6128a7b1d18552840
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-10 23:40:09 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (cpp)

---
+++ AG_Leetcode/problems/min_cost_climbing_stairs/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+       
+        
+        int n= cost.size();
+        vector<int> dp(n+1);
+        
+        
+        
+        for(int i =0; i<n; i++){
+            
+            
+            if(i<2){
+                
+                dp[i] = cost[i];
+                
+                
+            }
+            
+            else dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+            
+            
+        }
+        
+        
+        
+      return min(dp[n-1],dp[n-2]);
+        
+        
+        
+    }
+};
