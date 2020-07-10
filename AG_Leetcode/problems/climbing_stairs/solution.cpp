2d8ea7f623b0ae09b1e5ffe0d924542a1a243bd9
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-10 04:40:24 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- AG_Leetcode/problems/climbing_stairs/solution.cpp
+++ AG_Leetcode/problems/climbing_stairs/solution.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int climbStairs(int n) {
         
-       if(n==1){
-           return 1;
-       } 
+        int fib[n+1];
+        fib[0]=1;
+        fib[1] =1;
+        
+        for(int i =2; i<=n; i++){
+            
+            fib[i] = fib[i-1] + fib[i-2];
+            
+            
+            
+            
+            
+        }
         
         
-    int*dp = new int[n+1];
+    return fib[n];
         
         
-    dp[1] =1;
-    dp[2] = 2;
         
-    for(int i =3; i<=n; i++){
-        dp[i] = dp[i-1]+dp[i-2];
-    }
-    return dp[n];  
     }
 };
