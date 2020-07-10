c623655bd82b0c987dfa66814e3b6b917362a60d
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-10 02:52:55 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

---
+++ AG_Leetcode/problems/fibonacci_number/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        
+      int fibo[n+10];
+        
+       fibo[0]=0;
+        fibo[1]=1;
+        
+        for(int i =2; i<=n; i++){
+            fibo[i] = fibo[i-1] + fibo[i-2];
+            
+            
+        }
+        
+        
+      return fibo[n];  
+    }
+};
