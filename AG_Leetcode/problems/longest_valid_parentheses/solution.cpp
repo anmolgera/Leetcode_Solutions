87ae0f66d7ac841c95f619a4432ed5ff99e9daa7
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-16 12:54:54 +0000

    Sync LeetCode submission - Longest Valid Parentheses (cpp)

---
+++ AG_Leetcode/problems/longest_valid_parentheses/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+     stack<int> stk;
+        stk.push(-1);
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                stk.push(i);
+            } else {
+                stk.pop();
+                if (stk.empty()) {
+                    stk.push(i);
+                } else {
+                    ans = max(ans, i - stk.top());
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
