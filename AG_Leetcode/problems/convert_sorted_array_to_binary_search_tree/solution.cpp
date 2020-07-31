2394d74ec67451432373c9609fea26054d05c242
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-07-31 19:25:33 +0000

    Sync LeetCode submission - Convert Sorted Array to Binary Search Tree (cpp)

---
+++ AG_Leetcode/problems/convert_sorted_array_to_binary_search_tree/solution.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums, int start , int end ) {
+        
+        if(end<=start){
+            
+            return NULL;
+        }
+        
+        
+        int mid = start + (end-start)/2;
+        
+        TreeNode*root = new TreeNode(nums[mid]);
+        
+        root->left= sortedArrayToBST(nums,start,mid);
+        root->right = sortedArrayToBST(nums,mid+1,end);
+        
+        
+        
+        return root;
+        
+        
+    }
+    
+    
+      TreeNode* sortedArrayToBST(vector<int>& nums) {
+        
+     return   sortedArrayToBST(nums,0,nums.size());
+        
+        
+    }
+    
+};
