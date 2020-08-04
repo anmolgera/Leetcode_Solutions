fb9ec478eed40f6de18efdeed3c77325d09d5744
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-04 21:48:09 +0000

    Sync LeetCode submission - Trim a Binary Search Tree (cpp)

---
+++ AG_Leetcode/problems/trim_a_binary_search_tree/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        
+        if(root==NULL){
+            
+            return root;
+        }
+        
+        
+        if(root->val > R){
+            
+            return trimBST(root->left, L, R);
+        }
+        
+        if(root->val < L){
+            
+            return trimBST(root->right, L , R);
+        }
+        
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L , R);
+        
+        return root;
+    }
+};
