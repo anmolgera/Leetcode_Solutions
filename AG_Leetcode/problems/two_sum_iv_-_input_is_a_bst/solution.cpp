e91592ac386d2947e4aaed39a3c7aa20de326d32
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-05 01:11:56 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (cpp)

---
+++ AG_Leetcode/problems/two_sum_iv_-_input_is_a_bst/solution.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+    bool dfs(TreeNode* root, unordered_set<int>& s, int k){
+        
+        if(root == NULL){
+            
+            return false;
+        }
+        
+        
+        if(s.find(k-root->val)!=s.end()){
+            
+            return true;
+            
+            
+        }
+        
+        s.insert(root->val);
+        
+       return dfs(root->left,s,k) || dfs(root->right,s,k);
+        
+        
+        
+        
+    }
+    
+    
+    bool findTarget(TreeNode* root, int k) {
+        
+        
+        
+    unordered_set<int> s;
+    return dfs(root,s,k);
+        
+        
+        
+        
+        
+        
+    }
+};
