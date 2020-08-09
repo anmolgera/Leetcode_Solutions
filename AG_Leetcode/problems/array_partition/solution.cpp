c6369d191ada4e849c2a890d82446e60b5ffe59b
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-09 23:34:41 +0000

    Sync LeetCode submission - Array Partition (cpp)

---
+++ AG_Leetcode/problems/array_partition/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        
+        
+        int count =0;
+        sort(nums.begin(),nums.end());
+        
+        for(int i =0; i<nums.size(); i=i+2){
+            
+            count+=nums[i];
+        }
+        
+        return count;
+    }
+};
