71ca811f645c6972dc6a20e5e5a8894141493512
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-09 01:55:51 +0000

    Sync LeetCode submission - Subtract the Product and Sum of Digits of an Integer (cpp)

---
+++ AG_Leetcode/problems/subtract_the_product_and_sum_of_digits_of_an_integer/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        
+        int p=1;
+        int s=0;
+        
+        while(n!=0){
+            
+            s+=n%10;
+            p*=(n%10);
+            
+            n = n/10;
+            
+            
+        }
+        
+        return p-s;
+    }
+};
