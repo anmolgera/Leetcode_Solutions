d7332639caa176e9b10b628f0eb49f62a079afe4
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-12 04:39:31 +0000

    Sync LeetCode submission - Compare Version Numbers (cpp)

---
+++ AG_Leetcode/problems/compare_version_numbers/solution.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+        int n = version1.length();
+        int m = version2.length();
+        
+        int i =0;
+        int j =0;
+        int num1 =0;
+        int num2 =0;
+        
+        while(i<n || j<m){
+            
+           while(i<n && version1[i]!='.'){
+               
+               num1 = num1*10 + (version1[i]-'0');
+               i++;
+               
+           } 
+            
+                 while(j<m && version2[j]!='.'){
+               
+               num2 = num2*10 + (version2[j]-'0');
+                     j++;
+               
+           } 
+            
+           if(num1 > num2){
+               
+               return 1; 
+           } 
+            
+        if(num1 < num2){
+            
+            return -1; 
+        }
+         
+         num1 =0;
+         num2 =0;
+           i++;
+            j++;
+            
+        }
+        
+        return 0;
+        
+    }
+};
