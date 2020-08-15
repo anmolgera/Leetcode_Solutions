dcffc7160df9ea1a9794399f220fed8718b69bbd
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-15 09:54:02 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- AG_Leetcode/problems/number_of_islands/solution.cpp
+++ AG_Leetcode/problems/number_of_islands/solution.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
+    
+    
+    
     void dfs(vector<vector<char>>& grid, int i, int j){
         
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] =='0' ){
             
-            return;
-        }        
+            
+            return ;
+        }
         
-        grid[i][j] ='0';
+       grid[i][j] ='0';
         
-        dfs(grid,i-1,j);
-        dfs(grid,i,j-1);
+        dfs(grid,i,j-1); 
         dfs(grid,i+1,j);
+        dfs(grid, i-1, j);
         dfs(grid,i,j+1);
+        
+        
+        
+        
+        
         
         
         
@@ -23,31 +32,30 @@ public:
     
     
     
+    
+    
     int numIslands(vector<vector<char>>& grid) {
-        int  cnt =0;
-        for(int i =0; i<grid.size(); i++){
-            
-            for(int j =0; j<grid[0].size(); j++){
-                
-                if(grid[i][j]=='1'){
-                    
-                    
-                    dfs(grid,i,j);
-                    cnt++;
-                }
-                
-                
-                
-                
-            }
-            
-            
-            
-        }
+    int cnt =0;
+       for(int i =0; i<grid.size(); i++){
+           
+           for(int j =0; j<grid[0].size(); j++){
+               
+               if(grid[i][j]=='1'){
+                   
+                   dfs(grid,i,j);
+                   cnt++;
+               }
+               
+               
+               
+           }
+           
+           
+           
+           
+       } 
         
-        
-        return cnt;
-        
+     return cnt;   
         
     }
 };
