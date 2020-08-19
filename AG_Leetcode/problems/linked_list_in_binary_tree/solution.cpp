874be161c3f8075576e13a4c34621c5d3ef1928e
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-19 13:26:02 +0000

    Sync LeetCode submission - Linked List in Binary Tree (cpp)

---
+++ AG_Leetcode/problems/linked_list_in_binary_tree/solution.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool issame( TreeNode* root, ListNode* head){
+        
+     if(head == NULL){
+         return true;
+     } 
+        
+     if(root==NULL){
+         
+         return false;
+     }
+        
+     
+        
+     return (head->val == root->val ) && issame(root->left,head->next) ||   issame(root->right,head->next) &&(head->val == root->val ) ;  
+        
+        
+    }
+    
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        
+        
+   if(!root){
+       
+       return false;
+   }
+        
+    return issame(root,head) || isSubPath(head,root->left) || isSubPath(head,root->right);    
+        
+    }
+};
