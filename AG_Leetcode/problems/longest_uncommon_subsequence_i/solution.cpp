ac80dbeffbd3d9a14acae6aeddd28b28548235dd
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-08-20 15:24:24 +0000

    Sync LeetCode submission - Longest Uncommon Subsequence I (cpp)

---
+++ AG_Leetcode/problems/longest_uncommon_subsequence_i/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        
+       if(a==b){
+           return -1;
+       } 
+        
+        else {
+            
+            return max(a.length(),b.length());
+            
+            
+            
+            
+        }
+        
+        
+        
+    }
+};
