4ff5b7d3ba63dafda642b3b95880ca50aff99a31
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-11-20 12:20:59 +0000

    Sync LeetCode submission - Subsets (cpp)

--- AG_Leetcode/problems/subsets/solution.cpp
+++ AG_Leetcode/problems/subsets/solution.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs;
-        vector<int> sub;
-        subsets(nums, 0, sub, subs);
-        for(int i =0; i<subs.size(); i++){
+    
+    void dfs(vector<vector<int>> &v, vector<int> & nums, vector<int>  sub, int idx){
+        
+        
+        v.push_back(sub);
+        for(int i =idx; i<nums.size(); i++){
             
-            for(int j =0; j<subs[0].size(); j++){
-                
-                cout<<subs[i][j]<<" ";
-            }
-        }
-        return subs;
-    }
-private:
-    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
-        subs.push_back(sub);
-        for (int j = i; j < nums.size(); j++) {
-            sub.push_back(nums[j]);
-            subsets(nums, j + 1, sub, subs);
+            sub.push_back(nums[i]);
+            dfs(v,nums,sub,i+1);
             sub.pop_back();
+            
+            
+            
+            
+            
+            
         }
+        
         return;
+        
+        
+    }
+    
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> v;
+        vector<int> sub;
+         
+        dfs(v,nums,sub,0);
+        return v;
+        
+        
+        
+        
+        
     }
 };
