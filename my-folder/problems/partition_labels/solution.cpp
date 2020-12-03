10d3b98c5cc89665d2131ec72b32d3115b9ed227
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-03 23:08:37 +0000

    Sync LeetCode submission - Partition Labels (cpp)

---
+++ my-folder/problems/partition_labels/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        if(S.length()==0){
+            return {};
+        }
+        
+        map<int,int> mp;
+        
+        for(int i=0; i<S.length(); i++){
+            
+            mp[S[i]-'a']=i;
+        }
+        
+        int start =0;
+        int end =0;
+        vector<int> v;
+        for(int i =0; i<S.length(); i++){
+            
+            end = max(end,mp[S[i]-'a']);
+            if(end==i){
+                v.push_back(end-start+1);
+                start = end+1;
+            }
+            
+            
+        }
+        return v;
+    }
+};
