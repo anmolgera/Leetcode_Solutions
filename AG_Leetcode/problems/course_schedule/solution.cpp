841ee8064a796184d12ce5390b9028e8e58ba2ea
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-03 20:00:34 +0000

    Sync LeetCode submission - Course Schedule (cpp)

---
+++ AG_Leetcode/problems/course_schedule/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        
+        vector<vector<int>> graph(n);
+        vector<int> degree (n,0), bfs;
+        
+        for(auto e : prerequisites){
+            
+            graph[e[1]].push_back(e[0]);
+            degree[e[0]]++;
+            
+            
+            
+        }
+        
+        
+        for(int i =0; i<n; i++){
+            
+            if(degree[i]==0){
+                
+                bfs.push_back(i);
+            }
+        }
+        
+        
+        for(int i =0; i<bfs.size(); i++){
+            
+            for(auto j : graph[bfs[i]]){
+                
+                degree[j]--;
+                if(degree[j]==0){
+                    bfs.push_back(j);
+                }
+                
+            }
+            
+            
+        }
+        
+       return bfs.size()==n; 
+        
+        
+    }
+};
