d12d437d95e5066cd1586f51c9588c7a084a657c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-07 02:43:57 +0000

    Sync LeetCode submission - Odd Even Linked List (cpp)

---
+++ my-folder/problems/odd_even_linked_list/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+        if(!head){
+            return NULL;
+        }
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* connect = even;
+        
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+            
+            
+            
+        }
+        odd->next = connect;
+        return head;
+        
+    }
+};
