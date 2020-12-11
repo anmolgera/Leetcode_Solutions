333b7ac81cbf2a691ca6ef67abf3503f35d0b292
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-11 23:39:27 +0000

    Sync LeetCode submission - Minimum Remove to Make Valid Parentheses (cpp)

---
+++ my-folder/problems/minimum_remove_to_make_valid_parentheses/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        
+        for(int i =0; i<s.length(); i++){
+            
+            if(s[i]=='(' ){
+                st.push(i);
+            }
+            
+            else if(s[i]==')'){
+                
+                if(!st.empty()){
+                    st.pop();
+                }
+                
+                else {
+                    s[i] ='*';
+                }
+                
+            }
+        }
+            
+        while(!st.empty()){
+            s[st.top()] = '*';
+            st.pop();
+        }
+        string res = "";
+        for(int i =0; i<s.length(); i++){
+            if(s[i]!='*'){
+                res+=s[i];
+            }
+        }
+        return res;
+    }
+};
