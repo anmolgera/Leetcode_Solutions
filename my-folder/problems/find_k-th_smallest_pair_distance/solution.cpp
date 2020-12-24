2afa95e9cfe58339641512aceddf7606ce818c3e
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-24 20:34:23 +0000

    Sync LeetCode submission - Find K-th Smallest Pair Distance (cpp)

---
+++ my-folder/problems/find_k-th_smallest_pair_distance/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        int n = nums.size();
+        int N = 1000000;
+        vector<int> cnt(N,0);
+        for(int i =0; i<n-1; i++){
+            for(int j =i+1; j<n; j++){
+                cnt[abs(nums[i]-nums[j])]++;
+            }
+        }
+        
+        for(int i =0; i<N; i++){
+            
+            if(cnt[i]>=k){
+                return i;
+            }
+            
+            k-=cnt[i];
+        }
+        return 0;
+    }
+};
