546547cb22bba0da1db7b9e57bcec1dfce395f5f
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-27 20:58:36 +0000

    Sync LeetCode submission - Number of Dice Rolls With Target Sum (cpp)

---
+++ my-folder/problems/number_of_dice_rolls_with_target_sum/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+   //int sum =0;
+   
+int dp[31][1001] = {};
+int numRollsToTarget(int d, int f, int target, int res = 0) {
+  if (d == 0 || target <= 0) return d == target;
+  if (dp[d][target]>0) return dp[d][target]-1;
+  for (auto i = 1; i <= f; ++i)
+    res = (res + numRollsToTarget(d - 1, f, target - i)) % 1000000007;
+  dp[d][target] = res+1;
+  return res;
+}
+};
