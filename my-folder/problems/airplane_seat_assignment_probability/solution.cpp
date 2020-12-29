cdedcee1034c21b4d76338925f59d5f015a15ae9
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2020-12-29 21:40:22 +0000

    Sync LeetCode submission - Airplane Seat Assignment Probability (cpp)

---
+++ my-folder/problems/airplane_seat_assignment_probability/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+       if(n==1){
+           return 1;
+       }
+        
+      
+
+             return (double)1/2;
+        
+        
+    }
+};
