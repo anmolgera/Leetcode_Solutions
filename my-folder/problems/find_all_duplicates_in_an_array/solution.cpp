7ca264d6a785e65c5aecb7ca3242bc957bcc93df
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-01-22 20:47:02 +0000

    Sync LeetCode submission - Find All Duplicates in an Array (cpp)

---
+++ my-folder/problems/find_all_duplicates_in_an_array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for(int i =0; i<nums.size(); i++){
+            
+            int idx = abs(nums[i])-1;
+            if(nums[idx]<0){
+                res.push_back(idx+1);
+            }
+            
+            else nums[idx] =-nums[idx];
+            
+        }
+        
+        return res;
+    }
+};
