5949608105938da8184ec89c4c36b66132323f91
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-01-25 11:20:11 +0000

    Sync LeetCode submission - Search Insert Position (cpp)

---
+++ my-folder/problems/search_insert_position/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low =0;
+        int high = nums.size()-1;
+        
+        while(low<=high){
+            
+            int mid = low + (high-low)/2;
+            
+            if(target==nums[mid]){
+                return mid;
+            }
+            
+            if(nums[mid]<target){
+                
+                low = mid+1;
+            }
+            
+            else high = mid-1;
+            
+        }
+        
+      return high+1;
+    }
+};
