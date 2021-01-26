70f2a610954db92e9e65fcff2f30dc467acca2c9
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-01-26 13:25:37 +0000

    Sync LeetCode submission - Remove Element (cpp)

---
+++ my-folder/problems/remove_element/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int idx =0;
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i]!=val){
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx;
+    }
+};
