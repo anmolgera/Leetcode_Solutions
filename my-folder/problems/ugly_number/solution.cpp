4dea2c94b62b480c13d2591f859d720ff30e9777
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-01-27 20:33:55 +0000

    Sync LeetCode submission - Ugly Number (cpp)

---
+++ my-folder/problems/ugly_number/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        
+        if(num<1){
+            return false; // for negative numbers
+        }
+        
+        // for positive numsbers
+        while(num%2==0){
+            num/=2;
+        }
+        
+        while(num%3==0){
+            num/=3;
+        }
+        
+        while(num%5==0){
+            num/=5;
+        }
+        
+        if(num>1){
+            return false;
+        }
+        
+        else return true;
+    }
+};
