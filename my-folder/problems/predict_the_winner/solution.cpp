201337dc4170067ae81b23c3ba04b938f1d75373
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-02-02 01:07:24 +0000

    Sync LeetCode submission - Predict the Winner (cpp)

--- my-folder/problems/predict_the_winner/solution.cpp
+++ my-folder/problems/predict_the_winner/solution.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     
-    int check(vector<int> &nums, int s , int e, int turns){
-        
-        if(s==e){
-            return turns*nums[s];
-        }
-        
-        int a = turns*nums[s]+ check(nums,s+1,e,-turns);
-        int b = turns*nums[e]+ check(nums,s,e-1,-turns);
-        
-        return turns*max(turns*a,turns*b);
+    int check(vector<int> &nums, int s , int e){
+      
+      if(s==e){
+          return nums[e];
+      }
         
         
+      int a = nums[s] - check(nums,s+1,e);
+      int b = nums[e] - check(nums,s,e-1);
+        
+      return max(a,b);
+      
+       
     }
     
     
     
     
     bool PredictTheWinner(vector<int>& nums) {
-        return check(nums,0,nums.size()-1, 1) >=0;
+         
+        
+        return check(nums,0,nums.size()-1)>=0;     
+
+        
+
     }
 };
