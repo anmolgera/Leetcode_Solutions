4ba3bb8610dda4a636d38db0d03059e98ab943e8
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-02-23 17:46:19 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (cpp)

---
+++ my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if (root == NULL || root == p || root == q){
+            
+            return root;
+            
+       }
+        
+        auto left = lowestCommonAncestor(root->left,p,q);
+        auto right = lowestCommonAncestor(root->right,p,q);
+        
+        if(left && right){
+            return root;
+        }
+        
+        if(left){
+            
+            return left;
+        }
+        
+        
+        else return right;
+        
+        
+        
+        
+    }
+};
