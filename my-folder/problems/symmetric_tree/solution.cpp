b0bbd22b14c753fc22ccfe6c25bec71df992a1f4
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-02-26 14:48:29 +0000

    Sync LeetCode submission - Symmetric Tree (cpp)

---
+++ my-folder/problems/symmetric_tree/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool c(TreeNode* t1, TreeNode* t2){
+        
+        if(t1==NULL && t2==NULL){
+            return true;
+        }
+        
+        if(t1==NULL || t2==NULL){
+            return false;
+        }
+        
+        return t1->val==t2->val && c(t1->left,t2->right) && c(t1->right,t2->left);
+        
+        
+    }
+    
+    
+    
+    bool isSymmetric(TreeNode* root) {
+        return c(root,root);
+    }
+};
