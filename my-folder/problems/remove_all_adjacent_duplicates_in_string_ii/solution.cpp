0886259ae17182f5bcd0db4c8fe48c0dc8fe0d11
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-03-23 17:47:14 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (cpp)

--- my-folder/problems/remove_all_adjacent_duplicates_in_string_ii/solution.cpp
+++ my-folder/problems/remove_all_adjacent_duplicates_in_string_ii/solution.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        //int cnt =1;
-        int j =0;
+    
+    stack<pair<char,int>> st;
+    for(int i =0; i<s.length(); i++){
         
-        vector<int> cnt(s.size(),1);
-        for(int i =0; i<s.size(); i++,j++){
-            
-            s[j] = s[i];
-            
-            if(j>0 && s[j]==s[j-1]){
-                cnt[j] = cnt[j-1]+1;
-            }
-            
-            else cnt[j] =1;
-            
-            if(cnt[j]==k){
-                j-=k;
-            }
-            
-            
-            
+        if(st.empty() || st.top().first!=s[i]){
+            st.push({s[i],0});
         }
-        return s.substr(0,j);
+        
+        st.top().second++;
+        if(st.top().second==k){
+            st.pop();
+        }
+    }
+    string res ="";
+    while(st.size()){
+    
+    auto i = st.top();
+    //res+=string(i.second,i.first);
+    for(int j =0; j<i.second; j++){
+        res+=i.first;
+    }
+    st.pop();
+    
+    }
+        
+    reverse(res.begin(),res.end());
+    return res;
         
     }
 };
