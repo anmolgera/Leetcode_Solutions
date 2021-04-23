4812b3d2c2c9276896053fad9d2d0d39167cd2d7
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-04-23 15:13:31 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

--- my-folder/problems/partition_equal_subset_sum/solution.cpp
+++ my-folder/problems/partition_equal_subset_sum/solution.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
     
-   
+     
+    bool knapsack(vector<int>&nums,int n, int sum, vector<vector<int>> &dp){
+       
+    if(n==0 && sum!=0){
         
+        return false;
+    }
+    if(sum==0){
+        return true;
+    }
+    if(dp[n][sum]!=-1){
+        return dp[n][sum];
+    }
+        
+    if(nums[n-1]<=sum){
+        
+        return dp[n][sum] = knapsack(nums,n-1,sum-nums[n-1],dp) ||  knapsack(nums,n-1,sum,dp); 
+    }
+    
+    else {
+        return dp[n][sum] = knapsack(nums,n-1,sum,dp);
+    }
+        
+    }
+    
     
     bool canPartition(vector<int>& nums) {
-        
+    
+    int n = nums.size();
         int sum =0;
-        for(int i =0; i<nums.size(); i++){
-            sum+=nums[i];
-        }
+    for(int i =0; i<nums.size(); i++){
+    sum+=nums[i];
+    }
         
-        if(sum%2){
-            return false;
-            
-        }
+    if(sum%2){
+        return false;
+    }
         
-        sum = sum/2;
-        int n = nums.size();
-        int dp[n+1][sum+1];
-        dp[0][0] =1;
-        for(int i =1; i<=n; i++){
-            dp[i][0] = 1;
-        }
-        
-        for(int i =1; i<=sum; i++){
-            dp[0][i] =0;
-        }
-        
-        for(int i =1; i<=n; i++){
-            for(int j = 1; j<=sum; j++){
-                
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j] ||  dp[i-1][j-nums[i-1]];
-                }
-                
-                else dp[i][j] = dp[i-1][j];
-            }
-        }
-        
-        return dp[n][sum];
-        
+    else {
+        sum/=2;
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        return knapsack(nums,n,sum,dp);
+    }
+    
         
     }
 };
