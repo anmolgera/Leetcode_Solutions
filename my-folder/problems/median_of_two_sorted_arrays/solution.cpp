91f6923779c263ac253e0af79cff98771349d147
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-11 12:03:32 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (cpp)

--- my-folder/problems/median_of_two_sorted_arrays/solution.cpp
+++ my-folder/problems/median_of_two_sorted_arrays/solution.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int m = nums1.size();
-    int n = nums2.size();
-    
-    vector<int> ans;
-    for(int i =0; i<m; i++){
-        ans.push_back(nums1[i]);
-    }
-        
-    for(int i =0; i<n; i++){
-        ans.push_back(nums2[i]);
-    }
-        
-    sort(ans.begin(),ans.end());
-    
-    int t = ans.size();
-    if(t%2){
-    return ans[t/2];    
-    }
-    else {
-        return (double)(ans[t/2-1]+ ans[t/2])/2;
-    }
-    
-    
+             vector<int> ans;
+     for(int i =0; i<nums1.size(); i++){
+         ans.push_back(nums1[i]);
+     }
+
+     for(int i=0; i<nums2.size(); i++){
+         ans.push_back(nums2[i]);
+     }
+
+     sort(ans.begin(),ans.end());
+     int t = ans.size();
+     if(t%2){
+         return ans[t/2];
+     }
+
+     else{ double d = (double)(ans[t/2] + ans[t/2 -1])/2;
+            return d;
+            
+             }
     }
 };
