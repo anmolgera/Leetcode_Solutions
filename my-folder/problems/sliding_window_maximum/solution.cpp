9fead328c784559cd31934c981f3b5c876d11ea1
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-16 12:43:16 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- my-folder/problems/sliding_window_maximum/solution.cpp
+++ my-folder/problems/sliding_window_maximum/solution.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       
-        vector<int> vt;
-        deque<int> dq;
+     
+    vector<int> v;
+    deque<int> dq;
         
-        for(int i =0; i<nums.size(); i++){
-            
-            while(!dq.empty() && dq.back()<nums[i]){
-                dq.pop_back();
-            }
-            
-            dq.push_back(nums[i]);
-            
-            if(i>=k-1){
-                vt.push_back(dq.front());
-                //dq.pop_front();
-                if(nums[i-k+1]==dq.front()) dq.pop_front();
-            }
-            
+    for(int i =0; i<nums.size(); i++){
+        
+        while(!dq.empty() && dq.back()<nums[i]){
+            dq.pop_back();
+        }
+        
+        dq.push_back(nums[i]);
+        if(i>=k-1){
+            v.push_back(dq.front());
+            if(dq.front()==nums[i-(k-1)]) dq.pop_front();
             
         }
-        return vt;
+    }
+    
+    return v;
+        
+        
     }
 };
