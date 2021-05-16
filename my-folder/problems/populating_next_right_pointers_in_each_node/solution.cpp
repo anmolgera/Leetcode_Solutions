61a989b1505f82ac55314038464d9cd2a9f25e4a
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-16 19:53:31 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (cpp)

--- my-folder/problems/populating_next_right_pointers_in_each_node/solution.cpp
+++ my-folder/problems/populating_next_right_pointers_in_each_node/solution.cpp
@@ -1,26 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
 
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
 
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root == NULL){
-            return root;
+        
+    Node* par = root;
+    Node* cur = NULL;
+    if(!root) return root;
+    while(par->left){
+        cur = par;
+        while(cur){
+         cur->left->next = cur->right;
+         if(cur->next) cur->right->next = cur->next->left;
+         cur = cur->next;     
         }
-        Node*head = root;
-        while(root->left){
-             
-            Node* x = root;
-            while(x){
-                x->left->next = x->right;
-                if(x->next){
-                    x->right->next = x->next->left;
-                }
-                
-                x = x->next;
-             }
-            root = root->left;
-        }
-        return head;
+        par  = par->left;
     }
-      
+    return root;
+    }
 };
