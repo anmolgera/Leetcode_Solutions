1a6d063eea2741f2a38c6d623915c5a6f32d8722
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-17 20:15:53 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

--- my-folder/problems/maximum_subarray/solution.cpp
+++ my-folder/problems/maximum_subarray/solution.cpp
@@ -7,24 +7,27 @@ public:
         if(l>r){
             return INT_MIN;
         }
+                                // left
+                               // right
+                               // a[mid] 
         
         int m = l + (r-l)/2;
-        int ml =0;
-        int mr =0;
+        int pml =0;
+        int pmr =0;
         
         int lmax = maxSubArray(nums,l,m-1);
         int rmax = maxSubArray(nums,m+1,r);
         
-        for(int i =m-1, s =0; i>=0;i--){
+        for(int i =m-1, s =0; i>=l;i--){
             s+=nums[i];
-            ml = max(ml,s);
+            pml = max(pml,s);
         }
         
         for(int i =m+1,s=0; i<=r; i++){
             s+=nums[i];
-            mr = max(mr,s);
+            pmr = max(pmr,s);
         }
-      return max(max(lmax,rmax), ml+mr+nums[m]);  
+      return max(max(lmax,rmax), pml+pmr+nums[m]);  
         
     }
     
