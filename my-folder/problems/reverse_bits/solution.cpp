cf28112313a10a840a96c011adab356ff2d13c3c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-18 11:15:53 +0000

    Sync LeetCode submission - Reverse Bits (cpp)

---
+++ my-folder/problems/reverse_bits/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+    uint32_t ans =0;
+    int power = 31;
+    while(n){
+        ans = ans + ((n&1)<<power);
+        power--;
+        n>>=1;
+        
+    }
+    return ans;
+    }
+};
