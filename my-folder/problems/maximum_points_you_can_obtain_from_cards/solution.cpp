dff8976970851dfdcc283c83f934c638423642ee
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-18 09:35:46 +0000

    Sync LeetCode submission - Maximum Points You Can Obtain from Cards (cpp)

--- my-folder/problems/maximum_points_you_can_obtain_from_cards/solution.cpp
+++ my-folder/problems/maximum_points_you_can_obtain_from_cards/solution.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        vector<int> dp(k+1,0);
-         int n = cardPoints.size();
-        int sum =0;
-        for(int i =0; i<min(k,n); i++){
-            sum+=cardPoints[i];
+    
+    
+    /*int func(vector<int>&cardPoints, int k, int i, int j, vector<vector<int>> &dp){
+        
+        if(i>j || k==0){
+            return 0;
         }
         
-        if(n<=k){
-            return sum;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int mx = sum;
-       
-       for(int i = k-1; i>=0; i--){
-           sum-=cardPoints[i];
-           sum+=cardPoints[n-k+i];
-           mx = max(sum,mx);
-           
+        
+        return dp[i][j] = max(cardPoints[i] + func(cardPoints, k-1, i+1, j,dp), cardPoints[j] + func(cardPoints,k-1,i,j-1,dp));
+        
+    }
+    */
+    int maxScore(vector<int>& cardPoints, int k) {
+        int sum =0;
+        for(int i =0; i<k; i++){
+          sum+=cardPoints[i];  
+        }
+       int n = cardPoints.size(); 
+       int mx =sum;
+       int z = k-1;
+       for(int j =n-1; j>=n-k; j-- ){
+           sum-=cardPoints[z];
+           z--;
+           sum+=cardPoints[j];
+           mx = max(mx,sum);
        }
-        return mx;
+      return mx;
     }
 };
