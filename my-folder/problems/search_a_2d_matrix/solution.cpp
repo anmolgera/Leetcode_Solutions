b0596f65d139fa5cece87ba559adb2273d828b99
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-18 16:46:45 +0000

    Sync LeetCode submission - Search a 2D Matrix (cpp)

---
+++ my-folder/problems/search_a_2d_matrix/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    if(n==0 || m==0){
+        return false;
+    }
+    
+    int l =0;
+    int r = m*n-1;
+    int mid;
+    while(l<r){
+        mid = l+(r-l)/2;
+        if(matrix[mid/m][mid%m]==target) {
+           return true;
+        }
+        else if(matrix[mid/m][mid%m]>target){
+            r = mid-1;
+        }
+        else {
+            l = mid+1;
+        }
+        
+    }
+        
+    return matrix[l/m][l%m] == target;
+    
+    }
+};
