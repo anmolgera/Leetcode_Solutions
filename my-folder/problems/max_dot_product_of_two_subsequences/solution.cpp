5faef5c7e0975053211419e2098d3c63ad4bb11e
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-20 23:15:06 +0000

    Sync LeetCode submission - Max Dot Product of Two Subsequences (cpp)

---
+++ my-folder/problems/max_dot_product_of_two_subsequences/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {   
+        
+    int n = nums1.size();
+    int m = nums2.size();
+    vector<vector<int>>dp(n+1, vector<int>(m+1,-1*1e6));
+    for(int i =1; i<=n; i++){
+        for(int j =1; j<=m; j++){
+            dp[i][j] = max({dp[i-1][j-1], dp[i-1][j-1]+nums1[i-1]*nums2[j-1], dp[i-1][j],dp[i][j-1],nums1[i-1]*nums2[j-1]});
+        }
+    }
+     return dp[n][m];
+    }
+};
