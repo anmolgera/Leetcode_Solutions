9914896f8e5051b7a58f516785e3633b75b272f2
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-24 22:02:45 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (cpp)

--- my-folder/problems/minimum_size_subarray_sum/solution.cpp
+++ my-folder/problems/minimum_size_subarray_sum/solution.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int minSubArrayLen(int s, vector<int>& nums) {
+    int minSubArrayLen(int target, vector<int>& nums) {
         
-     int l =0;
-     int ans = INT_MAX;
-     int sum =0;
-     for(int r =0; r<nums.size(); r++){
-         
-         sum+=nums[r];
-         while(sum>=s){
-             ans = min(ans,r-l+1);
-             sum-=nums[l];
-             l++;
-         }
-         
-     }
-        if(ans==INT_MAX) return 0;
-        else return ans;
+    int i =0;
+    int j =0;
+    
+    int s = nums.size();
+    int sum =0;
+    int mn_len = INT_MAX;
+    while(j<s){
+        sum+=nums[j];
+        j++;
+       
+            while(sum>=target){
+               mn_len = min(mn_len,j-i);
+               sum-=nums[i];
+               i++;
+            }
+      
+    }
+        
+    return mn_len==INT_MAX ? 0 : mn_len;    
     }
 };
