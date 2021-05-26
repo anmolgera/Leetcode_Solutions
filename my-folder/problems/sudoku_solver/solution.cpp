79b776cee0862fa894b0c2713625b9d427b1e83c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-26 19:53:50 +0000

    Sync LeetCode submission - Sudoku Solver (cpp)

--- my-folder/problems/sudoku_solver/solution.cpp
+++ my-folder/problems/sudoku_solver/solution.cpp
@@ -1,60 +1,63 @@
 class Solution {
 public:
+    bool isvalid(vector<vector<char>>& board, char x, int row, int col){
+       for(int i =0; i<9; i++){
+         
+         if(board[i][col]==x){
+             return false;
+         }
+      }   
+        
+     for(int i =0; i<9; i++){
+         if(board[row][i]==x){
+             return false;
+         }
+     }
+     int y = row-row%3;  
+     int z = col-col%3;
+     for(int i =y; i<y+3; i++){
+         for(int j = z; j<z+3; j++){
+             if(board[i][j]==x){
+                 return false;
+             }
+         }
+     }
+        
+        
+        
+     return true;   
     
-    bool check (vector<vector<char>>& board, int row, int col , char c){
-        
-        for(int i =0; i<9; i++){
-            if(board[row][i]==c){
-                return false;
-            }
-        }
-        
-        for(int i =0; i<9; i++){
-            if(board[i][col]==c){
-                return false;
-            }
-        }
-        
-        int x = row-row%3;
-        int y = col-col%3;
-        
-        for(int i =0; i<3; i++){
-            for(int j =0; j<3; j++){
-                if(board[i+x][j+y]==c){
+    }
+    
+    
+bool solve( vector<vector<char>>& board)
+    {
+         
+        for(int i =0; i<board.size(); i++){
+            
+            for(int j =0; j<board[i].size(); j++){
+                
+                if(board[i][j]=='.'){
+                    for(char c ='1' ; c<='9'; c++){
+                        if(isvalid(board,c,i,j)){
+                        board[i][j] = c;
+                        if(solve(board)) return true;
+                        board[i][j] ='.';
+                        }
+                    }
                     return false;
                 }
             }
         }
-        return true;
+    
+      return true;
     }
     
-    bool solve(vector<vector<char>>& board){
-            for(int i =0; i<board.size(); i++){
-            for(int j =0; j<board[i].size(); j++){
-               if(board[i][j]=='.') {
-                   
-                   for(char c ='1'; c<='9'; c++){
-                       
-                       if(check(board,i,j,c)){
-                           board[i][j] = c;
-                           if(solve(board)) return true;
-                           board[i][j] ='.';
-                       }
-                   }
-                   return false;
-               }
-                
-            }
-        }
-        return true;
-    }
-    
-    void solveSudoku(vector<vector<char>>& board) {
-               
-        if(board.size()==0 || board[0].size()==0){
-            return;
-        }
-        
+    void solveSudoku( vector<vector<char>>& board){
+        int n = board.size();
+        int m = board[0].size();
+        if(n==0 || m==0) return;
         solve(board);
     }
+  
 };
