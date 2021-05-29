ce4a31b33a72040c28a5f1082e73c928b6f30c6d
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-05-29 21:43:00 +0000

    Sync LeetCode submission - Jump Game IV (cpp)

--- my-folder/problems/jump_game_iv/solution.cpp
+++ my-folder/problems/jump_game_iv/solution.cpp
@@ -1,47 +1,50 @@
- class Solution {
+class Solution {
 public:
+    unordered_map<int,vector<int>> mp;
     int minJumps(vector<int>& arr) {
-      unordered_map<int,vector<int>> mp;
-      int n = arr.size();
-      for(int i=0; i<arr.size(); i++){
-          mp[arr[i]].push_back(i);
-       }
-       vector<bool> visited(n,false);
-       visited[0] = true;
-       queue<int> q;
-       q.push(0);
-       int steps =0;
-        vector<int> next = {};
-       while(!q.empty()){
-           int s = q.size();
-           while(s--){
-               
-               int i = q.front();
-               //visited[i] = true;
-               q.pop();
-               if(i==n-1){
-                   return steps;
-                   break;
-               }
-                next = mp[arr[i]];
-              
-                next.push_back(i-1);
-                next.push_back(i+1);
-               
-              
-               for(int j =0; j<next.size(); j++){
-                   if( next[j]>=0 && next[j]<=n-1 && visited[next[j]]==false){
-                    q.push(next[j]);
-                    visited[next[j]]=true;
-                       //mp.erase(arr[next[j]]);
-                   }
-               }
-               mp.erase(arr[i]);
-               next = {};
-               
-             }
-           steps++;
-       }
-      return 0;
+    mp.clear();
+    for(int i =0; i<arr.size(); i++){
+        mp[arr[i]].push_back(i);
+    }
+    queue<int> q;
+    q.push(0);
+    int n = arr.size();
+    vector<int> vis(n,0);
+    for(int i =0; i<n; i++){
+        vis[i] =-1;
+    }
+        
+    vis[0] =1;
+    int level =0;
+    while(!q.empty()){
+    
+    int s = q.size();
+    while(s--){
+    auto p = q.front();
+    q.pop();
+    if(p==n-1) return level;
+
+    if(p+1<n && vis[p+1]==-1){
+        q.push(p+1);
+         vis[p+1] =1;
+    }
+        
+    if(p-1>=0 && vis[p-1]==-1){
+        q.push(p-1);
+         vis[p-1] =1;
+    }
+    auto v = mp[arr[p]];       
+    for(int i =0; i<v.size(); i++){
+    if(vis[v[i]]==-1){
+        q.push(v[i]);
+        vis[v[i]] =1;
+    }    
+    }    
+    mp.erase(arr[p]);    
+    }
+    level++;
+    }
+
+    return level;
     }
 };
