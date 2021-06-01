0a420c8d7fbc4bce893d4a811fbc162a63e3a92b
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-06-01 20:51:09 +0000

    Sync LeetCode submission - Validate Stack Sequences (cpp)

--- my-folder/problems/validate_stack_sequences/solution.cpp
+++ my-folder/problems/validate_stack_sequences/solution.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
     stack<int> st;
+    int n = pushed.size();
     int j =0;
-    for(int i =0; i<pushed.size(); i++){
-    st.push(pushed[i]);
-    while(st.size() && st.top()==popped[j]){
-        st.pop();
-        j++;
+    for(int i =0; i<n; i++){
+        st.push(pushed[i]);
+        while(!st.empty() && st.top()==popped[j]){
+            st.pop();
+            j++;
+        }
     }
-        
-    }
-    return st.empty();
+    return st.size()==0;
     }
 };
