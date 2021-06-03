f7dfc4b57a35616d1b44a75547cb17faa9a2056c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-06-03 19:58:03 +0000

    Sync LeetCode submission - Longest Palindromic Substring (cpp)

--- my-folder/problems/longest_palindromic_substring/solution.cpp
+++ my-folder/problems/longest_palindromic_substring/solution.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
+    
+    
+    
     string longestPalindrome(string s) {
-    if(s.length()<=1){
-        return s;
-    }
-    int n = s.length();    
-    vector<vector<int>> dp(n,vector<int>(n,false));
-        
-    for(int i =0; i<s.length(); i++){
+    int n =s.length();
+    vector<vector<bool>> dp(n,vector<bool>(n,false));
+    for(int i =0; i<n; i++){
         dp[i][i] = true;
     }
     
     int len =1;
-    int st =0;
-       
-    for(int start = s.length()-1; start>=0; start--){
+    int start =0;
         
-     for(int end = start+1; end<s.length(); end++){
-         
-         
-         if(s[start]==s[end]){
-             if(dp[start+1][end-1] || end-start==1){
-                 dp[start][end] = true;
-                 if(end-start+1>len){
-                     len = end-start+1;
-                     st = start;
-                 }
-             }
-         }
-         
-       }
+    for(int st = n-1; st>=0; st--){
+        for(int en = st+1; en<n; en++){
+           if(s[st]==s[en]){ 
+               
+             if( en-st==1 || dp[st+1][en-1]==true ){
+                dp[st][en] = true;
+                if(len<en-st+1){
+                    len = en-st+1;
+                    start = st;
+                }
+              }
+           }
+            
+        }
     }
-        
-    return s.substr(st,len);
-    }   
+    return s.substr(start,len);
+    }
 };
