7d52b051be105730c5ff962ddc3b32f38653cafe
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-06-09 07:19:03 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock/solution.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
     int mn = INT_MAX;
-    int ans = INT_MIN;
+    int mx =0;
     for(int i =0; i<prices.size(); i++){
-        mn = min(mn,prices[i]);
-        ans = max(ans,prices[i]-mn);
+        mn = min(prices[i],mn);
+        mx = max(mx,prices[i]-mn);
     }
-        
-    return ans;
+    return mx;
     }
 };
