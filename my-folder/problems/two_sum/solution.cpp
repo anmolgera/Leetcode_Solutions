f8ca6bb9ef0e941e597aaed2cbad0b612709712c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-07-08 21:44:43 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      
-    for(int i =0; i<nums.size()-1; i++){
-        for(int j = i+1; j<nums.size(); j++){
-            if(nums[i]+nums[j]==target){
-                return {i,j};
-            }
+    vector<int> v;
+    map<int,vector<int>> mp;
+    for(int i =0; i<nums.size(); i++){
+        
+    mp[nums[i]].push_back(i);
+        
+    if(mp.find(target-nums[i])!=mp.end()){
+        
+        if(mp[target-nums[i]][0]!=i){
+            v.push_back(i);
+            v.push_back(mp[target-nums[i]][0]);
+            return v;
+            
         }
     }
-    return {};
+        
+    }
+    return v;
     }
 };
