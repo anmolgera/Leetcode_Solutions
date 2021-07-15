3945a2498c41a1fb5c95fa34e714bf0583882f0c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-07-15 16:30:11 +0000

    Sync LeetCode submission - Pascal's Triangle (cpp)

--- my-folder/problems/pascal's_triangle/solution.cpp
+++ my-folder/problems/pascal's_triangle/solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-       
-        vector<vector<int>> res;
-        res.push_back({1});
-        int p =0;
-        for(int i =1; i<numRows; i++){
+    vector<vector<int>> res;
+    res.push_back({1});
+    int ch =0;
+    for(int i =1; i<numRows; i++){
+        vector<int> ans;
+        ans.push_back(1);
+        for(int j =1; j<=ch; j++ ){
             
-          vector<int> ans;
-          ans.push_back(1);
-          for(int j =1; j<=p; j++){
-              ans.push_back(res[i-1][j-1]+res[i-1][j]);
-          }
-          ans.push_back(1);
-          p++;
-          res.push_back(ans);
-            
-            
+             ans.push_back(res[i-1][j-1]+res[i-1][j]);   
         }
         
-       return res; 
-        
+        ans.push_back(1);
+        ch++;
+        res.push_back(ans); 
+    }
+    return res;  
     }
 };
