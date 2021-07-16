eb548b22f63370263843a4ca523ff7b3b5ef2f7c
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-07-16 06:21:23 +0000

    Sync LeetCode submission - N-th Tribonacci Number (cpp)

--- my-folder/problems/n-th_tribonacci_number/solution.cpp
+++ my-folder/problems/n-th_tribonacci_number/solution.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     
-    
-    int dp[38] ={};
+    int dp[38] = {};
     int tribonacci(int n) {
-        if(n==0){
-            return n;
-        }
-        
-        if(n==1 || n==2){
-            return 1;
-        }
-        
-        if(dp[n]!=0){
-            return dp[n];
-        }
-        
-        else return dp[n] = tribonacci(n-1)+tribonacci(n-2) + tribonacci(n-3);
+    if(n==0 || n==1 ) return n;
+    if(n==2) return 1;
+    if(dp[n]!=0) return dp[n];
+    return dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
     }
 };
