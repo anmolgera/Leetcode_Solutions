5f6b8091c6b09959e9531079d3d76852570b33bc
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-07-29 01:05:23 +0000

    Sync LeetCode submission - Course Schedule II (cpp)

--- my-folder/problems/course_schedule_ii/solution.cpp
+++ my-folder/problems/course_schedule_ii/solution.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& p) {
-        
-    int n = numCourses;    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    
+    int n = numCourses; 
     vector<vector<int>> graph(n);
     vector<int> indegree(n);
-    vector<int> ans;
-    for(int i =0; i<p.size(); i++){
-        
-        graph[p[i][1]].push_back(p[i][0]);
-        indegree[p[i][0]]++;
-        
+    
+    
+    for(int i =0; i<prerequisites.size(); i++){
+        graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        indegree[prerequisites[i][0]]++;
     }
         
     vector<int> bfs;
-    for(int i =0; i<indegree.size(); i++){
-        if(indegree[i]==0){
+    for(int i =0; i<n; i++){
+        if(indegree[i]==0) {
             bfs.push_back(i);
         }
     }
-        
+    
     for(int i =0; i<bfs.size(); i++){
         for(int j =0; j<graph[bfs[i]].size(); j++){
-            
             int x = graph[bfs[i]][j];
             indegree[x]--;
             if(indegree[x]==0){
                 bfs.push_back(x);
             }
         }
-    }    
-        
+    }
     if(bfs.size()==n) return bfs;
     else return {};
-        
     }
 };
