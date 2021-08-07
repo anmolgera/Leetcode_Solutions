9d9901d4e81d88fcd90043d95012d75825705893
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-08-07 14:26:00 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (cpp)

---
+++ my-folder/problems/two_sum_ii_-_input_array_is_sorted/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+      int l =0;
+    int r = numbers.size()-1;
+        
+    while(l<=r){
+        
+        if(numbers[l] + numbers[r]==target){
+            
+            
+            return vector <int > {l+1,r+1};
+            
+            
+        }
+        
+        else if(numbers[l] + numbers[r] <target){
+            
+            
+            l++;
+        }
+        
+     else {
+         r--;
+     }   
+        
+        
+        
+    }
+    
+        return {};
+    }
+};
