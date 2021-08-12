01050c89491f2fe69437681e73eefc7905bffe40
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-08-12 00:35:00 +0000

    Sync LeetCode submission - Find the Duplicate Number (cpp)

--- my-folder/problems/find_the_duplicate_number/solution.cpp
+++ my-folder/problems/find_the_duplicate_number/solution.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       if (nums.size() > 1)
-	{
-		int slow = nums[0];
-		int fast = nums[nums[0]];
-		while (slow != fast)
-		{
-			slow = nums[slow];
-			fast = nums[nums[fast]];
-		}
-
-		fast = 0;
-		while (fast != slow)
-		{
-			fast = nums[fast];
-			slow = nums[slow];
-		}
-		return slow;
-	}
-	return -1;
+     
+    if(nums.size()>1) {
+         
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+    
+    while(slow!=fast){
+        
+        slow = nums[slow];
+        fast = nums[nums[fast]];
     }
+   fast =0;     
+    while(slow!= fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+         
+    return slow;
+    }
+    
+    return -1;    
+    }
+
 };
