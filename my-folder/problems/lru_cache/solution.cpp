e8fd049a9a216c8297b70880418f733b538d0ae5
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2021-08-12 03:13:03 +0000

    Sync LeetCode submission - LRU Cache (cpp)

--- my-folder/problems/lru_cache/solution.cpp
+++ my-folder/problems/lru_cache/solution.cpp
@@ -1,20 +1,25 @@
 class LRUCache {
 public:
-    int c;
+    
+    int cap ;
+    
     list<int> dl;
-    unordered_map<int,pair<list <int> :: iterator, int >> mp;
+    
+    unordered_map<int,pair<list<int> :: iterator, int>> mp;
     
     LRUCache(int capacity) {
-        c = capacity;
+        cap = capacity;
     }
     
+    
     void movetofirst(int key){
+        
     dl.erase(mp[key].first);
     dl.push_front(key);
     mp[key].first = dl.begin();
         
-        
     }
+    
     
     int get(int key) {
     if(mp.find(key)==mp.end()) return -1;
@@ -23,28 +28,29 @@ public:
     }
     
     void put(int key, int value) {
-    if(mp.find(key)!=mp.end()){
-        mp[key].second = value;
-        movetofirst(key);
-    }  
+     
+    if(mp.find(key)==mp.end()){
+    dl.push_front(key);
+    mp[key] = {dl.begin(),value};
+    //movetofirst(key);
+    cap--;
+    if(cap<0){
+        mp.erase(dl.back());
+        cap++;
+        dl.pop_back();
+    }
+   
+    
+    
+    }
         
-     else {
-         
-      dl.push_front(key);
-      mp[key] = {dl.begin(),value};
-      movetofirst(key);
-      c--;
-      
-      if(c<0){
-      mp.erase(dl.back());
-      dl.pop_back();
-      c++;
-          
-      }
-         
-     } 
+    else {
+    mp[key].second = value;
+    movetofirst(key);
+    
         
-        
+    }
+    
     }
 };
 
