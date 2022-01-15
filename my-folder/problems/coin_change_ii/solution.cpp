fbcd672947e8b230c43d189ed9f744125a29e416
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-01-15 19:58:42 +0000

    Sync LeetCode submission - Coin Change II (cpp)

--- my-folder/problems/coin_change_ii/solution.cpp
+++ my-folder/problems/coin_change_ii/solution.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
+    int dp[301][5001];
+    int func(int amount, vector<int> coins, int n){
+    if(amount != 0 && n == 0) return 0;    
+    if(amount == 0) return 1;
+    if(dp[n][amount]!=-1){
+       return dp[n][amount];
+    }
+    if(coins[n-1] <= amount) {
+       return dp[n][amount] = func(amount - coins[n-1], coins, n)  + func(amount, coins, n-1);
+    }
+    
+       return dp[n][amount] = func(amount, coins, n-1);    
+        
+    }
+    
     int change(int amount, vector<int>& coins) {
     int n = coins.size();
-    int dp[n+2][amount+2];
-        
-    memset(dp,sizeof(dp),0);
-    
-    dp[0][0] =1;
-    for(int i =1; i<=n; i++){
-        dp[i][0] =1;
-    }
-        
-    for(int i =1; i<=amount; i++){
-        dp[0][i]=0;
-    }
-        
-    for(int i =1; i<=n; i++){
-        for(int j=1; j<=amount; j++){
-            dp[i][j] = dp[i-1][j];
-            if(coins[i-1]<=j){
-                dp[i][j]+=dp[i][j-coins[i-1]];
-            }
-        }
-    }
-    return dp[n][amount];
+    //vector<vector<int>> dp(n+1, vector<int> (amount+1,-1));
+    memset(dp,-1,sizeof(dp));
+    return func(amount, coins,n);    
     }
 };
