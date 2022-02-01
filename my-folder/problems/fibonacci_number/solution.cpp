cf8f1ef8575360ed2c51411ec3267a8587a1aaf7
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-02-01 00:09:23 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- my-folder/problems/fibonacci_number/solution.cpp
+++ my-folder/problems/fibonacci_number/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-    if(n==0 || n==1 ) return n;
-    else return fib(n-1)+fib(n-2);
+    if(n<=1) return n;
+    return fib(n-1) + fib(n-2);
     }
 };
