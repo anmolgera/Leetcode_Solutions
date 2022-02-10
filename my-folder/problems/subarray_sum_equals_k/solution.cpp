44e9c2467c4f3bd5ae7ec109ee55b17cb0e846c9
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-02-10 00:54:32 +0000

    Sync LeetCode submission - Subarray Sum Equals K (cpp)

--- my-folder/problems/subarray_sum_equals_k/solution.cpp
+++ my-folder/problems/subarray_sum_equals_k/solution.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-    
     map<int,int> mp;
     mp[0] = 1;
     int sum =0;
@@ -15,7 +14,6 @@ public:
     
         
     }
-    return count;
-        
+    return count;    
     }
 };
