0b93308a21642db6ce7790dc247beb1e68a21307
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-02-15 01:06:22 +0000

    Sync LeetCode submission - Single Number (cpp)

--- my-folder/problems/single_number/solution.cpp
+++ my-folder/problems/single_number/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
-        int s =0; 
+       int s =0; 
         
         for(int i =0; i<nums.size(); i++){
             
             s = s^nums[i];
             
         }
-        return s;
-        
+        return s;    
     }
 };
