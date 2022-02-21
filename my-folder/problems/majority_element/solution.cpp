a51725860f0d7953d23e7c5e6eb2b858911a16ca
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-02-21 03:34:21 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- my-folder/problems/majority_element/solution.cpp
+++ my-folder/problems/majority_element/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-     int ele = nums[0];
+    int ele = nums[0];
      int cnt =1;
      for(int i =1; i<nums.size(); i++){
      
