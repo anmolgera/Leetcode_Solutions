c88caa236c99cfa9ed76bf115f77494e589915fd
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-02-22 04:47:35 +0000

    Sync LeetCode submission - Excel Sheet Column Number (cpp)

--- my-folder/problems/excel_sheet_column_number/solution.cpp
+++ my-folder/problems/excel_sheet_column_number/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-    
-    int ans =0;
+        int ans =0;
     for(int i =0; i<columnTitle.size(); i++){
         
     ans = ans*26 + (columnTitle[i]-'A'+1);    
         
         
     }
-    return ans;
-    
+    return ans;    
     }
 };
