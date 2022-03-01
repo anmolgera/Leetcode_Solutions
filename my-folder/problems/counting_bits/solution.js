237c69f4e70fd913f99604c39e62747686f7f419
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-01 04:58:10 +0000

    Sync LeetCode submission - Counting Bits (javascript)

---
+++ my-folder/problems/counting_bits/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+let result = Array(n + 1).fill(0);
+  let offset = 1;
+  for (let i = 1; i < n + 1; i++) {
+    if (offset * 2 === i) {
+      offset = i;
+    }
+    result[i] = 1 + result[i - offset];
+  }
+
+  return result;    
+};
