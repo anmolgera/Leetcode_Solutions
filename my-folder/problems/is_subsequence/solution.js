4d920e811eb1a3f99b1669f48bd823efb33501ce
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-02 04:45:52 +0000

    Sync LeetCode submission - Is Subsequence (javascript)

---
+++ my-folder/problems/is_subsequence/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+let i = 0, j = 0;
+    while( j < t.length) {
+        if(s[i] === t[j]) {
+            i++;
+        }
+        j++;        
+    }
+    return i === s.length ? true: false;    
+};
