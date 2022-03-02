4863e0c6d9a57331d74ad2e52dab48e0be9d8cc4
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-02 04:44:14 +0000

    Sync LeetCode submission - Is Subsequence (cpp)

--- my-folder/problems/is_subsequence/solution.cpp
+++ my-folder/problems/is_subsequence/solution.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-    int i =0;
-    int j =0;
-    for( i =0; i<t.length() && j<s.length(); i++){
-     if(t[i]==s[j]){
-         j++;
-     }   
+    unordered_map<char, vector<int>> hash; //chars -> list of indices;
+        for(int i=0; i<t.length();i++) hash[t[i]].push_back(i);
         
-    }
+        int prev = -1;
+        for(auto c : s){
+            auto it = hash.find(c);
+            if(it == hash.end()) return false;
+            auto vec = it->second;
+            int pos = upper_bound(vec.begin(), vec.end(), prev) - vec.begin(); // find the first pos whose value is greater than prev
+            if(pos == vec.size()) return false;
+            prev = vec[pos];
+        }
+        return true;
         
-    return j == s.length();
     }
 };
