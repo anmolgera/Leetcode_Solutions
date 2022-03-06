9d413ad09525b4f6bf964c50e49d037d7df1b353
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-06 14:12:49 +0000

    Sync LeetCode submission - Count All Valid Pickup and Delivery Options (cpp)

--- my-folder/problems/count_all_valid_pickup_and_delivery_options/solution.cpp
+++ my-folder/problems/count_all_valid_pickup_and_delivery_options/solution.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    const int mod =1e9+7;
     int countOrders(int n) {
-    long res =1;    
-    for(int i =1; i<=n; i++){
-        res = res*((long)(i*2-1)*(i))%mod;
-    }
-    return res;
+    long long MODULO = 1e9 + 7;
+        long long ans = 1;
+        for (int i = 2; i <= n; i++) {
+            int spaceNum = (i-1)*2 + 1;
+            ans *= spaceNum * (spaceNum + 1) / 2;
+            ans %= MODULO;
+        }
+        return (int) ans;    
     }
 };
