0b701f4afa274ba029850dfb5dc408ffb8dcf397
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-07 09:50:35 +0000

    Sync LeetCode submission - Find Eventual Safe States (cpp)

---
+++ my-folder/problems/find_eventual_safe_states/solution.cpp
@@ -0,0 +1,23 @@
+
+enum Color {WHITE, GRAY, BLACK};
+class Solution {
+public:
+    bool dfs(int i, vector<int> & color, vector<vector<int>> & graph)
+    {
+        if(color[i]) return color[i] == BLACK;
+        color[i] = GRAY;
+        for(int & v : graph[i])
+            if(!dfs(v, color, graph)) return false;
+        color[i] = BLACK;
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph)
+    {
+        int n = int(graph.size());
+        vector<int> color(n, WHITE);
+        vector<int> result;
+        for(int i = 0; i < n; ++i)
+            if(dfs(i, color, graph)) result.push_back(i);
+        return result;
+    }
+};    
