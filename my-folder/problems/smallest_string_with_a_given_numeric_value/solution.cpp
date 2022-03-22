dbbac21b719e9d40cf9a8e744ff5b904ad39636e
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-22 06:27:23 +0000

    Sync LeetCode submission - Smallest String With A Given Numeric Value (cpp)

--- my-folder/problems/smallest_string_with_a_given_numeric_value/solution.cpp
+++ my-folder/problems/smallest_string_with_a_given_numeric_value/solution.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-     
     string res = string(n,'a');
     k-=n;
     while(k>0 && n>0){
@@ -10,7 +9,6 @@ public:
         k-=min(25,k);
     }
         
-    return res;
-        
+    return res;   
     }
 };
