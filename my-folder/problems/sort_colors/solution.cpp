13e5793931011f88a4449aaf3902b482844b14a2
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-03-23 15:30:39 +0000

    Sync LeetCode submission - Sort Colors (cpp)

--- my-folder/problems/sort_colors/solution.cpp
+++ my-folder/problems/sort_colors/solution.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-     int cnt=0;
-   int cnt1 =0;
-   int cnt2 =0;
-    int numsSize = nums.size();
-
-   for(int i =0; i<numsSize; i++){
-       if(nums[i]==0){
-           cnt++;
-       }
-
-       else if(nums[i]==1){
-           cnt1++;
-       }
-
-       else cnt2++;
-   }
-
-   for(int i =0; i<numsSize; i++){
-       if(cnt){
-           nums[i] = 0;
-           cnt--;
-       }
-
-       else if(cnt1){
-           nums[i] =1;
-           cnt1--;
-       }
-
-       else nums[i] =2;
-   }
-
-
-    
+    void sortColors(vector<int>& a) {
+    int n=a.size();
+        int l=0,m=0,r=n-1;
+        while(m<n && m<=r ){
+            if(a[m]==0){
+                swap(a[l++],a[m++]);
+                
+                
+            }
+            else if (a[m]==2){
+                swap(a[m],a[r--]);
+                
+               
+            }
+            else {
+                 m++;
+                
+                
+            }
+        }    
     }
 };
