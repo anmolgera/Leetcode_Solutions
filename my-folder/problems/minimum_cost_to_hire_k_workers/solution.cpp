4ff68d27228ff884258c292b7195b036db52d6f9
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-04-07 20:13:02 +0000

    Sync LeetCode submission - Minimum Cost to Hire K Workers (cpp)

---
+++ my-folder/problems/minimum_cost_to_hire_k_workers/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& q, vector<int>& w, int K) {
+        vector<vector<double>> workers;
+        for (int i = 0; i < q.size(); ++i)
+            workers.push_back({(double)(w[i]) / q[i], (double)q[i]});
+        sort(workers.begin(), workers.end());
+        double res = DBL_MAX, qsum = 0;
+        priority_queue<int> pq;
+        for (auto worker: workers) {
+            qsum += worker[1], pq.push(worker[1]);
+            if (pq.size() > K) qsum -= pq.top(), pq.pop();
+            if (pq.size() == K) res = min(res, qsum * worker[0]);
+        }
+        return res;   
+    }
+};
