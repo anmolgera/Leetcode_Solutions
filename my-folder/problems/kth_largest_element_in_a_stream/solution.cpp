e84d12724e63eec4d4d75c81e710828d4877fa64
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-04-08 13:48:07 +0000

    Sync LeetCode submission - Kth Largest Element in a Stream (cpp)

--- my-folder/problems/kth_largest_element_in_a_stream/solution.cpp
+++ my-folder/problems/kth_largest_element_in_a_stream/solution.cpp
@@ -18,9 +18,3 @@ public:
         return pq.top();
     }
 };
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
