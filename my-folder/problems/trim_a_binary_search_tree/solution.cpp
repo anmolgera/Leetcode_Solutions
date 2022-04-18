e44b73c66367b1f3f66bed31b7ab3a65258d8652
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-04-18 14:42:19 +0000

    Sync LeetCode submission - Trim a Binary Search Tree (cpp)

--- my-folder/problems/trim_a_binary_search_tree/solution.cpp
+++ my-folder/problems/trim_a_binary_search_tree/solution.cpp
@@ -12,21 +12,17 @@
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
+    if(!root) return nullptr;
         
-    if(root == NULL) return root;
-    if(root->val < low) {
+        if(root->val < low) {
+            return trimBST(root->right, low, high);
+        } else if (root->val > high) {
+            return trimBST(root->left, low, high);
+        }
         
-        return trimBST(root->right,low,high);
-    }
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
         
-    if(root->val > high){
-        
-        return trimBST(root->left,low,high);
-    }
-        
-    root->left = trimBST(root->left,low,high);
-    root->right = trimBST(root->right,low,high);
-    return root;
-        
+        return root;    
     }
 };
